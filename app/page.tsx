7911bef4308aed883bd214d2fc21fd5e55f3a354
Farmeurimmo <farmeurimmo@gmail.com>
2023-06-20 17:32:23 +0200

    Mise à jour du contenu & nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-    {name: "Projets", href: "/projets"},
+    {name: "Blog & Projets", href: "/projets"},
     {name: "Contact", href: "/contact"},
 ];
 
