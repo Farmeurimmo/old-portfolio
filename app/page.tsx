fb15706303ede2a7d1505017dc651858e0a7b0a9
Farmeurimmo <farmeurimmo@gmail.com>
2023-07-04 17:11:01 +0200

    Suppression alignement sur la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 
             <div
                 className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"/>
-            <div className="my-16 text-center animate-fade-in">
+            <div className="my-16 text-left animate-fade-in">
                 <h2 className="text-sm text-zinc-400 ">
                     Développeur Java, API Minecraft : Spigot/Paper/Purpur/Bungee/Velocity, Web (HTML,CSS,JS/TS), C++,
                     SQL, Python
