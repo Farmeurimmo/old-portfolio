31a4a448e240b5d2f25e193867644d86ee572997
Farmeurimmo <contact@farmeurimmo.fr>
2023-12-29 12:44:00 +0100

    Content update

--- app/page.tsx
+++ app/page.tsx
@@ -53,17 +53,6 @@ export default function Home() {
             <div
                 className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"/>
             <div className="my-16 text-left animate-fade-in" id="home">
-                <h2 className="text-2xl font-bold text-zinc-300">
-                    A new version of the website is coming soon (see it on <Link
-                    target="_blank"
-                    href="https://v4.farmeurimmo.fr"
-                    className="underline duration-500 hover:text-zinc-300"
-                >
-                    here
-                </Link>.)
-                </h2>
-                <br/>
-                <br/>
                 <p className="text-sm text-zinc-400 ">
                     {t('desc_dev')}
                     <br/>
@@ -74,10 +63,29 @@ export default function Home() {
                     {t('desc_am_ai')}
                     <br/>
                     <br/>
-                    {t('desc_provider')}
                     <br/>
                     <br/>
                     {t('devat_noctis')}
+                    <Link
+                        target="_blank"
+                        href="https://lastjourney.fr"
+                        className="underline duration-500 hover:text-zinc-300"
+                    >
+                        LastJourney
+                    </Link>
+                    <br/>
+                    <br/>
+                    (not public) {t('devat_noctis')}
+                    <Link
+                        target="_blank"
+                        href="https://dymuno.com"
+                        className="underline duration-500 hover:text-zinc-300"
+                    >
+                        Dymuno
+                    </Link>
+                    <br/>
+                    <br/>
+                    (not public) {t('devat_noctis')}
                     <Link
                         target="_blank"
                         href="https://noctis.rip"
