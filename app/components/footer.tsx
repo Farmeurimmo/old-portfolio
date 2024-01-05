04c3842999ea647b036c4059738caaa32e512395
Farmeurimmo <contact@farmeurimmo.fr>
2024-01-05 12:33:06 +0100

    Update of year to 2024

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
                 href="https://farmeurimmo.fr"
                 className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
             >
-                Copyright © 2023 Farmeurimmo
+                Copyright © 2024 Farmeurimmo
             </Link>
             <Link
                 target="_blank"
